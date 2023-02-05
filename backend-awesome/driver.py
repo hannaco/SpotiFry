7022f118f58501500e34266cad1af9d05ec7da57
josephpinto <124000783+josephpinto@users.noreply.github.com>
2023-02-05 02:34:04 +0000

    Create initial Flask backend API infra

--- backend-awesome/driver.py
+++ backend-awesome/driver.py
@@ -1,2 +1,20 @@
-import spotipy
+from flask import Flask
+from flask_cors import CORS
+  
+# Initializing flask app
+app = Flask(__name__)
+CORS(app) 
+  
 
+@app.route('/data')
+def get_time():
+  
+    # Returning an api for showing in  reactjs
+    return {
+        "data": 'hello world!', 
+        }
+  
+      
+# Running app
+if __name__ == '__main__':
+    app.run(debug=True)
