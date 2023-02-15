e2b1513392236c0b2ca8a0ffdd4e741d3c3e5fc4
Ziqi Xu <ziqixu091@gmail.com>
2023-02-15 15:10:49 -0800

    Update HomePage.js
    Add download link button, need backend link to be provided

--- frontend-dope/src/HomePage.js
+++ frontend-dope/src/HomePage.js
@@ -15,30 +15,30 @@ const HomePage = () => {
 
         const SAVED_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/tracks`;
         const USER_PROFILE_ENDPOINT = `https://api.spotify.com/v1/me`;
-        
+
         const getUserInfo = async () => {
             const { data } = await axios.get(USER_PROFILE_ENDPOINT, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
             });
             // console.log(data)
             setUserProfile(data);
         };
-    
+
         const searchSavedSongs = async () => {
             const { data } = await axios.get(SAVED_TRACKS_ENDPOINT, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-            params: {
-                limit: 10,
-            },
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                params: {
+                    limit: 10,
+                },
             });
             setSavedSongs(data.items);
         };
 
-        if(token === '' || token === null) {
+        if (token === '' || token === null) {
             return;
         } else {
             getUserInfo();
@@ -48,50 +48,62 @@ const HomePage = () => {
 
     const SavedSongs = () => {
         return savedSongs.map((favorite) => (
-        <div key={favorite.track.id}>
-            <p>{favorite.track.name}</p>
-            {/* {favorite.track.images[0].url} */}
-        </div>
+            <div key={favorite.track.id}>
+                <p>{favorite.track.name}</p>
+                {/* {favorite.track.images[0].url} */}
+            </div>
         ));
     };
 
     const LogOut = () => {
         window.localStorage.removeItem("token");
-        navigate('/', {replace: true});
+        navigate('/', { replace: true });
+    }
+
+    const DownLoad = {
+
     }
 
     return (
         <> {/* if not logged in (no token) navigate back to login page */}
-        {! window.localStorage.getItem("token") ? <Navigate replace to='/'/> 
-        :
-        <div>
-            <div className="homepage">
-                <button className="logout-button" onClick={LogOut}>
-                Sign Out
-                </button>
+            {!window.localStorage.getItem("token") ? <Navigate replace to='/' />
+                :
                 <div>
-                    <h2>Hi, {userProfile.display_name}</h2>
-                    {/* 
+                    <div className="homepage">
+                        <button className="logout-button" onClick={LogOut}>
+                            Sign Out
+                        </button>
+                        <div>
+                            <h2>Hi, {userProfile.display_name}</h2>
+                            {/* 
                         https://daveceddia.com/react-before-render/ 
                         check for null/undefined value since render happens before data is ready
                     */}
-                    {userProfile && userProfile.images && userProfile.images[0] ? (
-                        <img className="profileImg" src={userProfile.images[0].url} alt="" />
-                    ) : (
-                        <div>[No Profile Image]</div>
-                    )}
+                            {userProfile && userProfile.images && userProfile.images[0] ? (
+                                <img className="profileImg" src={userProfile.images[0].url} alt="" />
+                            ) : (
+                                <div>[No Profile Image]</div>
+                            )}
+                        </div>
+                        <div>
+                            <h4>Your default playlist</h4>
+                            <button
+                                className="logout-button"
+                                type="button"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    window.location.href = 'http://google.com'; /* NEED a link from BACKEND */
+                                }}
+                            > Click here </button>
+                            <SavedSongs />
+                        </div>
+                    </div>
+                    <NavBar />
                 </div>
-                <div>
-                    <h4>Your Saved Songs</h4>
-                    <SavedSongs />
-                </div>
-            </div>
-            <NavBar/>
-        </div>
-        }
-        
+            }
+
         </>
-        
+
     );
 }
 
