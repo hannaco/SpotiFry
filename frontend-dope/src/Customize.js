e8c3ee99019978c5637a291ed789a5902ea54b7a
Ziqi Xu <ziqixu091@gmail.com>
2023-02-19 17:32:06 -0800

    Slider mui

--- frontend-dope/src/Customize.js
+++ frontend-dope/src/Customize.js
@@ -1,12 +1,44 @@
 import "./Customize.css";
-import Slider from "./Slider";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import { useState } from "react";
 
 function Customize() {
+    const DiscreteSlider = ({ name }) => {
+
+        const [numParam, setnumParam] = React.useState([]);
+
+        return (
+            <div className="slidecontainer">
+                <h6> {name} </h6>
+                <Slider
+                    defaultValue={1}
+                    valueLabelDisplay="auto"
+                    step={1}
+                    marks
+                    min={1}
+                    max={10}
+                />
+            </div>
+        );
+    }
+
     return (
         <div className="customize">
             <h4>Only a few steps away from your personalized playlist...</h4>
-            <Slider />
-
+            <div className="slidecontainer">
+                <Box sx={{ width: 300 }}>
+                    <DiscreteSlider name="Danceability" />
+                    <DiscreteSlider name="Accousticness" />
+                    <DiscreteSlider name="Energy" />
+                    <DiscreteSlider name="Instrumentalness" />
+                    <DiscreteSlider name="Loudness" />
+                    <DiscreteSlider name="Valence" />
+                    <DiscreteSlider name="Tempo" />
+                    <DiscreteSlider name="Musicality" />
+                </Box>
+            </div>
         </div>
 
     );
