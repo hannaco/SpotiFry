d75d19b098676272b1bb3027e9fc81ad712a7c49
Ziqi Xu <ziqixu091@gmail.com>
2023-02-19 21:35:29 -0800

    Input+Slider

--- frontend-dope/src/Customize.js
+++ frontend-dope/src/Customize.js
@@ -3,6 +3,12 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import { useState } from "react";
+import TextField from '@mui/material/TextField';
+import { alpha, styled } from '@mui/material/styles';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import { OutlinedInputProps } from '@mui/material/OutlinedInput';
+import Button from '@mui/material/Button';
 
 function Customize() {
     const DiscreteSlider = ({ name }) => {
@@ -11,7 +17,6 @@ function Customize() {
 
         return (
             <div className="slidecontainer">
-                <h6> {name} </h6>
                 <Slider
                     defaultValue={1}
                     valueLabelDisplay="auto"
@@ -19,26 +24,102 @@ function Customize() {
                     marks
                     min={1}
                     max={10}
+                    sx={{ width: 300, color: 'success.main', }}
                 />
             </div>
         );
     }
 
+    const CssTextField = styled(TextField)({
+        '& input:valid + fieldset': {
+            borderColor: 'green',
+            borderWidth: 2,
+        }
+    });
+
     return (
         <div className="customize">
             <h4>Only a few steps away from your personalized playlist...</h4>
-            <div className="slidecontainer">
-                <Box sx={{ width: 300 }}>
-                    <DiscreteSlider name="Danceability" />
-                    <DiscreteSlider name="Accousticness" />
-                    <DiscreteSlider name="Energy" />
-                    <DiscreteSlider name="Instrumentalness" />
-                    <DiscreteSlider name="Loudness" />
-                    <DiscreteSlider name="Valence" />
-                    <DiscreteSlider name="Tempo" />
-                    <DiscreteSlider name="Musicality" />
+            <div className="inputcontainer">
+                <Button
+                    variant="contained"
+                    component="label"
+                    color="success"
+                    size="small"
+                >
+                    Upload your playlist cover
+                    <input
+                        type="file"
+                        hidden
+                    />
+                </Button>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                        border: 1, borderColor: 'green', borderRadius: '10px',
+                        display: 'flex', flexDirection: 'column', m: 2,
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <CssTextField
+                        required
+                        id="outlined-required"
+                        label="Playlist Name"
+                        defaultValue="Hello World"
+                        varient="filled"
+                        sx={{ input: { color: 'white' } }}
+                        InputLabelProps={{
+                            style: { color: 'green' },
+                        }}
+                    />
+                    <CssTextField
+                        required
+                        id="outlined-required"
+                        label="Artist"
+                        defaultValue="Beyonce"
+                        sx={{ input: { color: 'white' } }}
+                        InputLabelProps={{
+                            style: { color: 'green' },
+                        }}
+                    />
+                    <CssTextField
+                        required
+                        id="outlined-required"
+                        label="Genre"
+                        defaultValue="Hip-pop"
+                        sx={{ input: { color: 'white' } }}
+                        InputLabelProps={{
+                            style: { color: 'green' },
+                        }}
+                    />
                 </Box>
             </div>
+            <div className="slidecontainer">
+                <Box sx={{
+                    width: 300, display: 'block', typography: 'body1', m: 2
+                }}>
+                    Danceability <DiscreteSlider name="Danceability" />
+                    Accousticness <DiscreteSlider name="Accousticness" />
+                    Energy <DiscreteSlider name="Energy" />
+                    Instrumentalness <DiscreteSlider name="Instrumentalness" />
+                    Loudness <DiscreteSlider name="Loudness" />
+                    Valence <DiscreteSlider name="Valence" />
+                    Tempo <DiscreteSlider name="Tempo" />
+                    Musicality <DiscreteSlider name="Musicality" />
+                </Box>
+            </div>
+            <Button
+                onClick={() => {
+                    alert('clicked');
+                }}
+                variant="contained"
+                color="success"
+                size="big"
+            >
+                Generate
+            </Button>
         </div>
 
     );
