f02f4d18a429a7f86c657cab879f8e2c6d4e28a5
Hanna Co <hannaco@ucla.edu>
2023-02-28 20:59:51 -0800

    storing values
    storing slider values into state

--- frontend-dope/src/Customize.js
+++ frontend-dope/src/Customize.js
@@ -5,24 +5,39 @@ import Slider from '@mui/material/Slider';
 import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
 import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Customize() {
-    const DiscreteSlider = () => {
+    const [Danceability, setDanceability] = useState(1);
+    const [Accousticness, setAccousticness] = useState(1);
+    const [Energy, setEnergy] = useState(1);
+    const [Instrumentalness, setInstrumentalness] = useState(1);
+    const [Loudness, setLoudness] = useState(1);
+    const [Valence, setValence] = useState(1);
+    const [Tempo, setTempo] = useState(1);
+    const [Musicality, setMusicality] = useState(1);
+    const [Name, setName] = useState("Hello World");
+    const [Artist, setArtist] = useState("Beyonce");
+    const [Genre, setGenre] = useState("Hip-popH");
 
-        return (
-            <div className="slidecontainer">
-                <Slider
-                    defaultValue={1}
-                    valueLabelDisplay="auto"
-                    step={1}
-                    marks
-                    min={1}
-                    max={10}
-                    sx={{ width: 300, color: 'success.main', }}
-                />
-            </div>
-        );
-    }
+    // const DiscreteSlider = (props) => {
+
+    //     return (
+    //         <div className="slidecontainer">
+    //             <Slider
+    //                 defaultValue={1}
+    //                 valueLabelDisplay="auto"
+    //                 value={props.val}
+    //                 onChange={props.func}
+    //                 step={1}
+    //                 marks
+    //                 min={1}
+    //                 max={10}
+    //                 sx={{ width: 300, color: 'success.main', }}
+    //             />
+    //         </div>
+    //     );
+    // }
 
     const CssTextField = styled(TextField)({
         '& input:valid + fieldset': {
@@ -91,6 +106,14 @@ function Customize() {
                     <button className="generate"
                         onClick={() => {
                             alert('clicked');
+                            console.log(Danceability)
+                            console.log(Accousticness)
+                            console.log(Energy)
+                            console.log(Instrumentalness)
+                            console.log(Loudness)
+                            console.log(Valence)
+                            console.log(Tempo)
+                            console.log(Musicality)
                         }}
                     >
                         Generate
@@ -100,14 +123,110 @@ function Customize() {
                     <Box sx={{
                         width: 300, display: 'block', typography: 'body1', m: 2
                     }}>
-                        Danceability <DiscreteSlider name="Danceability" />
-                        Accousticness <DiscreteSlider name="Accousticness" />
-                        Energy <DiscreteSlider name="Energy" />
-                        Instrumentalness <DiscreteSlider name="Instrumentalness" />
-                        Loudness <DiscreteSlider name="Loudness" />
-                        Valence <DiscreteSlider name="Valence" />
-                        Tempo <DiscreteSlider name="Tempo" />
-                        Musicality <DiscreteSlider name="Musicality" />
+                        Danceability 
+                            <div className="Danceability">
+                                <Slider
+                                    defaultValue={1}
+                                    valueLabelDisplay="auto"
+                                    onChange={(e) => setDanceability(e.target.value)}
+                                    step={1}
+                                    marks
+                                    min={1}
+                                    max={10}
+                                    sx={{ width: 300, color: 'success.main', }}
+                                />
+                            </div>
+                        Accousticness 
+                            <div className="Accousticness">
+                                <Slider
+                                    defaultValue={1}
+                                    valueLabelDisplay="auto"
+                                    onChange={(e) => setAccousticness(e.target.value)}
+                                    step={1}
+                                    marks
+                                    min={1}
+                                    max={10}
+                                    sx={{ width: 300, color: 'success.main', }}
+                                />
+                            </div>
+                        Energy 
+                            <div className="Energy">
+                                <Slider
+                                    defaultValue={1}
+                                    valueLabelDisplay="auto"
+                                    onChange={(e) => setEnergy(e.target.value)}
+                                    step={1}
+                                    marks
+                                    min={1}
+                                    max={10}
+                                    sx={{ width: 300, color: 'success.main', }}
+                                />
+                            </div>
+                        Instrumentalness 
+                            <div className="Instrumentalness">
+                                <Slider
+                                    defaultValue={1}
+                                    valueLabelDisplay="auto"
+                                    onChange={(e) => setInstrumentalness(e.target.value)}
+                                    step={1}
+                                    marks
+                                    min={1}
+                                    max={10}
+                                    sx={{ width: 300, color: 'success.main', }}
+                                />
+                            </div>
+                        Loudness 
+                            <div className="Loudness">
+                                <Slider
+                                    defaultValue={1}
+                                    valueLabelDisplay="auto"
+                                    onChange={(e) => setLoudness(e.target.value)}
+                                    step={1}
+                                    marks
+                                    min={1}
+                                    max={10}
+                                    sx={{ width: 300, color: 'success.main', }}
+                                />
+                            </div>
+                        Valence 
+                            <div className="Valence">
+                                <Slider
+                                    defaultValue={1}
+                                    valueLabelDisplay="auto"
+                                    onChange={(e) => setValence(e.target.value)}
+                                    step={1}
+                                    marks
+                                    min={1}
+                                    max={10}
+                                    sx={{ width: 300, color: 'success.main', }}
+                                />
+                            </div>
+                        Tempo 
+                            <div className="Tempo">
+                                <Slider
+                                    defaultValue={1}
+                                    valueLabelDisplay="auto"
+                                    onChange={(e) => setTempo(e.target.value)}
+                                    step={1}
+                                    marks
+                                    min={1}
+                                    max={10}
+                                    sx={{ width: 300, color: 'success.main', }}
+                                />
+                            </div>
+                        Musicality 
+                            <div className="Musicality">
+                                <Slider
+                                    defaultValue={1}
+                                    valueLabelDisplay="auto"
+                                    onChange={(e) => setMusicality(e.target.value)}
+                                    step={1}
+                                    marks
+                                    min={1}
+                                    max={10}
+                                    sx={{ width: 300, color: 'success.main', }}
+                                />
+                            </div>
                     </Box>
                 </div>
             </div>
