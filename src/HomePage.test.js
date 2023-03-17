a212d5db678b64aa00003987d18500d70fb0ad1f
josephpinto <124000783+josephpinto@users.noreply.github.com>
2023-03-17 20:46:32 +0000

    remove userEvent import

--- src/HomePage.test.js
+++ src/HomePage.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import HomePage from "./HomePage";
 import {BrowserRouter as Router} from 'react-router-dom';
 import '@testing-library/jest-dom'
